2dad5bf24208957b6c3681a5017590f0e8bcafad
Erik Hollensbe <github@hollensbe.org>
2017-04-20 08:01:16 -0700

    Makefile,build.rb: support new TESTRUN environment parameter to run a specific test.
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ checks:
 	@bash checks.sh
 
 run_test: checks
-	docker run --rm box-builder/tarutil
+	docker run -e TESTRUN=${TESTRUN} --rm box-builder/tarutil
 
 test: install_box build run_test
 
--- build.rb
+++ build.rb
@@ -10,4 +10,4 @@ workdir path
 run "set -e; if [ ! -d vendor ]; then go get github.com/LK4D4/vndr && vndr; fi"
 
 # this entrypoint clear works around a box 0.5.1 bug.
-set_exec entrypoint: [], cmd: ["/bin/sh", "-c", "go test -cover -v ./..."]
+set_exec entrypoint: ["/usr/bin/env"], cmd: ["/bin/sh", "-c", %q[go test -cover -run "${TESTRUN}" -v ./...]]
